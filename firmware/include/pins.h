e1b74253080d87f7caf0a987315aeef43bace0e2
Josef Duchesne <josefduchesne@outlook.com>
2023-10-08 23:17:44 -0400

    set up pin definitions

---
+++ firmware/include/pins.h
@@ -0,0 +1,64 @@
+#include <Arduino.h>
+
+#ifndef __PINS_H
+#define __PINS_H
+
+// Free pins:
+// A5
+// ~D3 ~D9 
+// D7
+// D12 (SPI CIPO)
+
+const int OPinUltraData = 11;
+const int OPinUltraClock = 13;
+const int OPinUltraLatch = 10;
+const int IPinUltraEcho = 8;
+
+const int OPinStepperLeftStep = A0;
+const int OPinStepperLeftDir = A1;
+const int OPinStepperRightStep = A2;
+const int OPinStepperRightDir = A3;
+
+const int OPinServoA = 5;
+const int OPinServoB = 6;
+
+const int IOPinI2CData = 19;
+const int OPinI2CClock = 18;
+
+const int OPinBluetoothTX = 2;
+const int IPinBluetoothRX = 4;
+
+const int IPinIR = A4;
+
+void initializePins() {
+
+  // Ultrasonic array
+  pinMode(OPinUltraData, OUTPUT);
+  pinMode(OPinUltraClock, OUTPUT);
+  pinMode(OPinUltraLatch, OUTPUT);
+  pinMode(IPinUltraEcho, INPUT);
+
+  // Stepper motor drivers
+  pinMode(OPinStepperLeftStep, OUTPUT);
+  pinMode(OPinStepperLeftDir, OUTPUT);
+  pinMode(OPinStepperRightStep, OUTPUT);
+  pinMode(OPinStepperRightDir, OUTPUT);
+
+  // Servo signals
+  pinMode(OPinServoA, OUTPUT);
+  pinMode(OPinServoB, OUTPUT);
+
+  // I2C bus
+  pinMode(IOPinI2CData, OUTPUT);
+  pinMode(OPinI2CClock, OUTPUT);
+
+  // Bluetooth software serial
+  pinMode(OPinBluetoothTX, OUTPUT);
+  pinMode(IPinBluetoothRX, INPUT);
+
+  // Black/White IR sensor  
+  pinMode(IPinIR, INPUT);
+  
+}
+
+#endif // __PINS_H
--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,5 +1,23 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
 
-void setup() {}
+#include "pins.h"
+
+SoftwareSerial Bluetooth;
+
+void setup() {
+
+  // Setup pin modes
+  initializePins();
+
+  // Setup communication interfaces
+  Serial.begin(9600);
+  Wire.begin(9600); // Use default pins
+  SPI.begin(); // Use default pins
+  Bluetooth = SoftwareSerial(IPinBluetoothRX, OPinBluetoothTX);
+
+}
 
 void loop() {}
